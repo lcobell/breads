2f5e7266fdd02183be0901e74404dd6fa35d40d1
lcobell <lcobell@gmail.com>
2022-03-23 18:01:53 -0500

    sending index and show data

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+// CONFIGURATION
+require("dotenv").config();
+const PORT = process.env.PORT;
+const app = express();
+
+// Routes
+
+app.get("/", (req, res) => {
+  res.send("Welcome to an Awesome App about Breads");
+});
+
+// Breads
+const breadsController = require("./controllers/breads_controller.js");
+app.use("/breads", breadsController);
+
+// Listen
+
+app.listen(PORT, () => {
+  console.log("nomming at port", PORT);
+});
