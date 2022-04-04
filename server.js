35a56c948bd5bbf77f36268dcf1fba8050d21a96
lcobell <lcobell@gmail.com>
2022-04-04 17:22:28 -0500

    fixed the delete route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 // DEPENDENCIES
+// require mongoose
+const mongoose = require("mongoose");
+
 const methodOverride = require("method-override");
 
 // CONFIGURATION
@@ -7,6 +10,14 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 const app = express();
 
+// Mongo URI
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("connected to mongo: ", process.env.MONGO_URI);
+  }
+);
 // MIDDLEWARE
 app.set("views", __dirname + "/views");
 app.set("view engine", "jsx");
